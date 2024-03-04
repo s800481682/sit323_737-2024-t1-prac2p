80600f4bc396486abe6eaa3572c99adc72e952c9
s800481682 <s800481682@deakin.edu.au>
2024-03-04 19:53:41 +1100

    feat: make app.js into a real web server

--- Week1/app.js
+++ Week1/app.js
@@ -1,1 +1,10 @@
-console.log("Week-1 ")
+var express = require("express")
+var app = express()
+
+// The "Hello Word" endpoint
+app.get('/', function (req, res) {
+    res.send('hello world')
+})
+
+// Listen!
+app.listen(3000)
