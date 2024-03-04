1b10c5cd3cc84bc1768c342686890b9a74a6d1c5
s800481682 <s800481682@deakin.edu.au>
2024-03-04 19:55:08 +1100

    fix: format javascript files

--- Week1/app.js
+++ Week1/app.js
@@ -1,10 +1,10 @@
-var express = require("express")
-var app = express()
+var express = require("express");
+var app = express();
 
 // The "Hello Word" endpoint
-app.get('/', function (req, res) {
-    res.send('hello world')
-})
+app.get("/", function (req, res) {
+  res.send("hello world");
+});
 
 // Listen!
-app.listen(3000)
+app.listen(3000);
