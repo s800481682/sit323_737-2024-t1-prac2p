cf231baaf9ad39ee8bef960d5eb0be9568274995
s800481682 <s800481682@deakin.edu.au>
2024-03-04 20:02:15 +1100

    feat: add /addTwoNumber endpoint:

--- Week1/app.js
+++ Week1/app.js
@@ -1,10 +1,30 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const res = require("express/lib/response");
+const app = express();
+
+// addTwoNumber will take two numbers and return their sum
+const addTwoNumber = (n1, n2) => {
+  return n1 + n2;
+};
+
+// The "Addition" endpoint
+app.get("/addTwoNumber", (req, res) => {
+  const n1 = parseInt(req.query.n1);
+  const n2 = parseInt(req.query.n2);
+  const result = addTwoNumber(n1, n2);
+  res.json({ statuscode: 200, data: result });
+});
 
 // The "Hello Word" endpoint
 app.get("/", function (req, res) {
   res.send("hello world");
 });
 
+// Rando code
+console.log(addTwoNumber(19, 12));
+
 // Listen!
-app.listen(3000);
+const port = 3040;
+app.listen(port, () => {
+  console.log("hello i'm listening to port" + port);
+});
